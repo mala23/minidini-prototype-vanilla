867b975a3b386ea5716b148d191c7d627e268765
Colin Nico Schmid <mail@mala23.me>
2018-06-06 16:31:18 +0200

    prepare form for writing to db

--- server.js
+++ server.js
@@ -23,24 +23,6 @@ app.set('view engine', '.hbs')
 app.use(bodyParser.json())
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-function dummyData() {
-  Item.count().exec((err, count) => {
-    if (count > 0) {
-      return;
-    }
-
-    const description1 = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.' 
-
-    const item1 = new Item({ name: 'Microwave', icon: 'https://s3.eu-central-1.amazonaws.com/minidini/images/icons/icon_microwave-white.svg', category: 'Household Appliance', description: description1 })
-
-    Item.create([item1], (error) => {
-      if (!error) {
-        console.log(ready)
-      }
-    })
-  })
-}
-
 app.get('/', function(req, res) {
   Item.find({}, function(err, items) {
     res.render('inventory', { title: 'MiniDini – Inventory', items: items })
